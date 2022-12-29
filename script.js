92bf119f869ce072d7474bfd260419ae2e368c4d
abdussalam-mustapha <abdussalammustapha07@gmail.com>
2022-12-29 12:45:00 +0100

    new updates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let btn = document.querySelector(".reflink")
 let submitBtn = document.querySelector(".btn")
 let ref = document.querySelector(".links-container")
-let val = document.querySelector(".textVal")
+let val = document.querySelector(".form-data")
 
 
 const refLink = document.createElement("p")
@@ -19,13 +19,37 @@ btn.onclick = (e) => {
     e.preventDefault()
 }
 
-submitBtn.onclick = (e) => {
-    const userInputs = {
-        tkn : val.value
-    }
+// submitBtn.onclick = (e) => {
+//     const userInputs = {
+//         tkn : val.value
+//     }
     
-    localStorage.setItem("user", JSON.stringify(userInputs))
-    e.preventDefault()
+//     localStorage.setItem("user", JSON.stringify(userInputs))
+//     e.preventDefault()
 
-}
+// }
 
+// function send_data(){
+//     var form_data = document.getElementsByClassName("form-data"); //retrieve filled form data
+//     var i;
+//     var data = [];
+//     for(i=0; i<form_data.length; i++){
+//       data.push(form_data[i].value);
+//     }
+//     google.script.run.saveData(data); // send to google app script
+//     document.getElementById("form").style.display = "none"; // make form invisible
+//     document.getElementById("completion-msg").style.display = "block"; // Optional if you want to give a completion feedback!
+//   }
+
+//   function doGet() {
+//     return HtmlService.createHtmlOutputFromFile('index');
+//   }
+  
+//   // App Script function to save data to sheet
+//   function saveData(data) {
+//     var sheet = SpreadsheetApp.getActiveSheet();
+//     sheet.appendRow(data);
+//   }
+
+//   AKfycbxMaSYcf1h0yXQwBlCjfRdYbK3-fl6aFdt6CU5ylaclzGF9_uZ2EVv2slGDJOzqyuTv
+//   https://script.google.com/macros/s/AKfycbxMaSYcf1h0yXQwBlCjfRdYbK3-fl6aFdt6CU5ylaclzGF9_uZ2EVv2slGDJOzqyuTv/exec
