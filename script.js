7e3cace17541ea8b6041bb21d016e99826a67f98
abdussalam-mustapha <abdussalammustapha07@gmail.com>
2022-12-29 10:48:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector(".reflink")
+let ref = document.querySelector(".links-container")
+
+
+const refLink = document.createElement("p")
+
+function createRef(baseUrl, referralId) {
+    refLink.innerText = `${baseUrl}?ref=${referralId}`;
+    ref.appendChild(refLink)
+}
+
+let referralId = ("" + Math.random()).substring(2, 8);
+let baseUrl = ""
+
+btn.onclick = (e) => {
+    createRef(baseUrl, referralId)
+    e.preventDefault()
+}
+
