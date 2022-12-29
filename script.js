e6ea7584f1ed45752abe52b355f3ef01336db596
abdussalam-mustapha <abdussalammustapha07@gmail.com>
2022-12-29 15:49:30 +0100

    new updates

--- script.js
+++ script.js
@@ -21,37 +21,5 @@ btn.onclick = (e) => {
 
 
 
-// submitBtn.onclick = (e) => {
-//     const userInputs = {
-//         tkn : val.value
-//     }
-    
-//     localStorage.setItem("user", JSON.stringify(userInputs))
-//     e.preventDefault()
 
-// }
-
-// function send_data(){
-//     var form_data = document.getElementsByClassName("form-data"); //retrieve filled form data
-//     var i;
-//     var data = [];
-//     for(i=0; i<form_data.length; i++){
-//       data.push(form_data[i].value);
-//     }
-//     google.script.run.saveData(data); // send to google app script
-//     document.getElementById("form").style.display = "none"; // make form invisible
-//     document.getElementById("completion-msg").style.display = "block"; // Optional if you want to give a completion feedback!
-//   }
-
-//   function doGet() {
-//     return HtmlService.createHtmlOutputFromFile('index');
-//   }
-  
-//   // App Script function to save data to sheet
-//   function saveData(data) {
-//     var sheet = SpreadsheetApp.getActiveSheet();
-//     sheet.appendRow(data);
-//   }
-
-//  AKfycbxOC7ajGig8teTKoQmzmeO2sNyOvtfyNUfouNW4dzAV8R-gKHBlHgcokQK9Er6hgjZZfA
-//   https://script.google.com/macros/s/AKfycbxOC7ajGig8teTKoQmzmeO2sNyOvtfyNUfouNW4dzAV8R-gKHBlHgcokQK9Er6hgjZZfA/exec
+// AKfycbwoNcQUX9K8fjb2Czj2HkDPVZx9kcDPeeroVccmJv1zplsE97M2vHF-1WXyL2A7UNjD
